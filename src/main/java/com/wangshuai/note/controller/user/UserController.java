e4c687d017c3b4517124dcc6d22de3b46ef02452
wangshuai <wangshuai@guahao.com>
2018-05-23 15:38:01 +0800

    Signed-off-by: wangshuai <wangshuai@guahao.com>

--- src/main/java/com/wangshuai/note/controller/user/UserController.java
+++ src/main/java/com/wangshuai/note/controller/user/UserController.java
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) 2001-2017 GuaHao.com Corporation Limited. All rights reserved. 
- * This software is the confidential and proprietary information of GuaHao Company. 
- * ("Confidential Information"). 
- * You shall not disclose such Confidential Information and shall use it only 
- * in accordance with the terms of the license agreement you entered into with GuaHao.com.
- */
 package com.wangshuai.note.controller.user;
 
 import com.wangshuai.note.vo.NoteResultVO;
